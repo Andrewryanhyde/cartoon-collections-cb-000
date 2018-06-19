a29e9361f5360478baaee722f5abcf44ef4bd235
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-19 14:55:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,9 +18,9 @@ def long_planeteer_calls(array)
     i = 0
     while i < array.length
       if array[i].length > 4
-        return true
+        true
       else array[i].length <= 4
-        return false
+        false
       end
       i += 1
     end
