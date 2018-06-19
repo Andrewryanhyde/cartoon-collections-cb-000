44c0b1ac41c6d36c6a1dd47cdce5aa45ea4ac224
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-19 14:08:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,12 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.split(" ")
+  array.map do |element|
+    element.capitalize
+  end
+  array.join(" ")
 end
 
 def long_planeteer_calls# code an argument here
