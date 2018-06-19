1cbf5346f045bbb7dbed7ecd71ec5ea487a6c01a
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-19 14:33:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,11 +5,13 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map! do |element|
-    element.capitalize
-    element + "!"
+  newArray = []
+  i = 0
+  while i < array.length
+    newArray << array[i].capitalize + "!"
+    i += 1
   end
-  array
+  newArray
 end
 
 def long_planeteer_calls# code an argument here
