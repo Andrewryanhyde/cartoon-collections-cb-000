02b9be2d0249519d7e76e93c6ac48a3bd64d58bb
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-19 14:59:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,22 +16,12 @@ end
 
 def long_planeteer_calls(array)
     i = 0
-    newArray = []
-    while i < array.length
-      if array[i].length > 4
-        newArray.push("true")
-      else array[i].length < 4
-        newArray.push("false")
-      end
-      i += 1
-    end
-
-    if newArray.any? = true
-      return true
-    else
+    if array.any? {|i| i.length > 4}
+      return true 
+    else 
       return false
     end
-end
+  end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
